b21fea61fef41168c998797f646b12c8a41913e2
Phillip Junior <Phillipj1514@users.noreply.github.com>
2017-07-05 16:41:26 -0500

    Update Team_Ja_Fgc_code.java

--- Team_Ja_Fgc_code.java
+++ Team_Ja_Fgc_code.java
@@ -66,6 +66,7 @@ public class Team_Ja_Fgc_code extends LinearOpMode {
         //variable initialization
         double left = 0.0;
         double right = 0.0;
+        double motion = 0.0;
         int minimum_distance = 40; // the minimum distance of the proximity / color sensor
 
         // Send message to tablet showing that robot is ready and wait until start is pressed
@@ -85,7 +86,7 @@ public class Team_Ja_Fgc_code extends LinearOpMode {
                 color_value_argb = color_sensor.argb();
                 left = -gamepad1.left_stick_y;
                 right = -gamepad1.right_stick_y;
-
+                motion = -gamepad2.left_stick_y;
 
                 //controller command function and functionality
 
@@ -95,21 +96,16 @@ public class Team_Ja_Fgc_code extends LinearOpMode {
 
                 sorting_System(minimum_distance,1.0,1.0,1.0); //sorting system
 
-                // manipulate the storage chamber gates
-                if(gamepad2.left_trigger > 0.5)
-                   blueGateOpen(); 
-                
-                if(gamepad2.right_trigger > 0.5)
-                   orangeGateOpen();
-                
-                if(gamepad2.left_bumper)
-                    blueGateClose();
+                blueGateOpen(0.2,1000); //function to open the blue ball storage whenever the button is clicked
 
-                if(gamepad2.right_bumper)
-                    orangeGateClose();
+                blueGateClose(0.2,3000); //function to close the blue ball storage whenever the button is clicked
 
-                linear_Slide(1.0); //linear slide movement function
-                
+                orangeGateOpen(0.7); //function to open the orange ball storage whenever the button is clicked
+
+                orangeGateClose(0); //function to close the orange ball storage whenever the button is clicked
+
+                linear_Slide(motion); //linear slide movement function
+
                 // logging information to user that is necessary
                 telemetry.addData("Left wheel: ", "%.2f", left);
                 telemetry.addData("Right wheel:", "%.2f", right);
@@ -146,7 +142,7 @@ public class Team_Ja_Fgc_code extends LinearOpMode {
         left_Motor.setPower(left);
         right_Motor.setPower(right);
 
-        // brake mode
+        // let the robot brake whenever it stop moving
         if(left == 0)
             left_Motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
@@ -157,8 +153,8 @@ public class Team_Ja_Fgc_code extends LinearOpMode {
     private void ball_Elevator(int minimum_distance, double power){
         //elevator system functionalities
         if (color_value_alpha <= minimum_distance) {  // check if balls are in sensing range
-            if (gamepad1.b) {
-                elevator_on = true; //set true to show that the elevator is running
+            if (gamepad2.b || gamepad1.right_bumper) {
+                elevator_on = true; //set  true to show that the elevator is running
                 startElevator(power);
             }
         } else {
@@ -167,24 +163,34 @@ public class Team_Ja_Fgc_code extends LinearOpMode {
             color_sensor_motor2.setPower(0);
         }
 
-        if (gamepad1.left_bumper) { // turn off button for elevator
+        if (gamepad2.a || gamepad1.left_bumper) { // turn off button for elevator
             elevator_on = false;
             stopElevator();
         }
     }
 
+    private void stopElevator(){
+        color_sensor_motor.setPower(0);
+        color_sensor_motor2.setPower(0);
+    }
+
+    private void startElevator(double power){
+        color_sensor_motor.setPower(-power);
+        color_sensor_motor2.setPower(-power);
+    }
+
     private void sorting_System(int minimum_distance, double orange_ball_position, double blue_ball_position,double power){
         // The codes for the sorting action
-        if (gamepad1.x) { // Turn on manual mode for the sorting system
+        if (gamepad2.x) { // Turn on manual mode for the sorting system
             manual_mode = true;
             telemetry.addData("say", "Manual mode on");
         }
-        if (gamepad1.y) { // turn off manual mode for the sorting sytem
+        if (gamepad2.y) { // turn off manual mode for the sorting sytem
             manual_mode = false;
             telemetry.addData("say", "Manual mode off");
         }
         //manual control for servo
-        if (gamepad1.dpad_left) { // Turn the servo to the blue ball storage
+        if (gamepad2.right_stick_x > 0.5) { // Turn the servo to the blue ball storage
 
             color_servo.setPosition(blue_ball_position);
             sleep(1000);
@@ -195,7 +201,7 @@ public class Team_Ja_Fgc_code extends LinearOpMode {
                 startElevator(power);//Anika
             }
         }
-        if (gamepad1.dpad_right) { //Turn the servo to the orange ball storage
+        if (gamepad2.right_stick_x < -0.5) { //Turn the servo to the orange ball storage
 
             color_servo.setPosition(-orange_ball_position);
             sleep(1000);
@@ -207,12 +213,12 @@ public class Team_Ja_Fgc_code extends LinearOpMode {
             }
         }
         // Mode to collect just blue balls
-        if (gamepad1.left_trigger >= 0.5) { // Turn the servo to the blue ball storage and hold
+        if (gamepad2.dpad_up) { // Turn the servo to the blue ball storage and hold
             manual_mode = false;
             color_servo.setPosition(0.7);
             telemetry.addData("say", "Blue ball only mode on");
         }
-        if (gamepad1.right_trigger >= 0.5) { // return the servo position to the neutral zone
+        if (gamepad2.dpad_down) { // return the servo position to the neutral zone
             manual_mode = true;
             color_servo.setPosition(0.5);
             telemetry.addData("say", "Blue ball only mode off");
@@ -245,22 +251,15 @@ public class Team_Ja_Fgc_code extends LinearOpMode {
 
     }
 
-    private void linear_Slide(double power){
+    private void linear_Slide(double motion){
         // Lift system controls
-        if (gamepad2.left_stick_y > 0.5) { // raise the lift up
-            linearSlide_motor1.setPower(power);
-            linearSlide_motor2.setPower(power);
-            linearSlide_motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        } else {
+        if(motion != 0){
+            linearSlide_motor1.setPower(motion);
+            linearSlide_motor2.setPower(motion);
+        }else{
             stopLinearSlide();
         }
 
-        if (gamepad2.left_stick_y < -0.5) { // pull the lift down
-            linearSlide_motor1.setPower(-power);
-            linearSlide_motor2.setPower(-power);
-        } else {
-            stopLinearSlide();
-        }
     }
 
     private void stopLinearSlide(){
@@ -272,45 +271,42 @@ public class Team_Ja_Fgc_code extends LinearOpMode {
         linearSlide_motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
-    // gate functions, Anika
-    private void blueGateOpen(){
+    private void blueGateOpen(double power, int time){
+        if(gamepad2.left_trigger > 0.5){
         bGate_open = true;
-        blueGate.setPower(1);
-        sleep(2000);
+        blueGate.setPower(-power);
+        sleep(time);
         blueGate.setPower(0);
-
         stopElevator();
+        }
     }
 
-    private void orangeGateOpen(){
-        oGate_open = true;
-        orangeGate_servo.setPosition(1);
-        
-        stopElevator();
+    private void blueGateClose(double power, int time){
+        if(gamepad2.left_bumper) {
+            bGate_open = false;
+            blueGate.setPower(power);
+            sleep(time);
+            blueGate.setPower(0);
+        }
     }
 
-    private void blueGateClose(){
-        bGate_open = false;
-        blueGate.setPower(-1);
-        sleep(2000);
-        blueGate.setPower(0);
+    private void orangeGateOpen(double position){
+        if(gamepad2.right_trigger > 0.5) {
+            oGate_open = true;
+            orangeGate_servo.setPosition(position);
+
+            stopElevator();
+        }
     }
 
-    private void orangeGateClose(){
-        oGate_open = false;
-        orangeGate_servo.setPosition(0);
+    private void orangeGateClose(double position){
+        if(gamepad2.right_bumper) {
+            oGate_open = false;
+            orangeGate_servo.setPosition(position);
+        }
     }
 
-    public void stopElevator(){
-        color_sensor_motor.setPower(0);
-        color_sensor_motor2.setPower(0);
-    }
-
-    public void startElevator(double power){
-        color_sensor_motor.setPower(-power);
-        color_sensor_motor2.setPower(-power);
-    }
-    
+    //The Processor ticking time code
     /***
      * waitForTick implements a periodic delay. However, this acts like a metronome
      * with a regular periodic tick. This is used to compensate for varying
